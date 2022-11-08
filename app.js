f1117566dea6dbc530ca170bfab05821c0adf952
Vitor Jaguat <vitorbutkus@gmail.com>
2022-11-08 15:54:26 -0300

    adjust script, improve algorithm

--- app.js
+++ app.js
@@ -1,44 +1,49 @@
 
 
 const timerF = function () {
-    const fimBozo = new Date(2023, 1, 1);
-    // console.log(fimBozo)
-    // console.log(fimBozo.getDate())
+    const fimBozo = new Date('January 01 2023 00:00:00');
     const now = new Date();
+    const diff = fimBozo - now;
 
-    const second = 1000
-    const minute = second * 60
-    const hour = minute * 60
-    const day = hour * 24
+    const timerDays = Math.floor(diff/1000/60/60/24);
+    const timerHours = Math.floor(diff/1000/60/60) % 24;
+    const timerMinutes = Math.floor(diff/1000/60) % 60;
+    const timerSeconds = Math.floor(diff/1000) % 60;
 
-    const distance = fimBozo - now;
-    const timerDays = Math.floor((distance / day) - 31);
-    // console.log(distance/day)
-    const timerHours = Math.floor(distance % day / hour);
-    const timerMinutes = Math.floor(distance % hour / minute);
-    const timerSeconds = Math.floor(distance % minute / second)
+    // const second = 1000
+    // const minute = second * 60
+    // const hour = minute * 60
+    // const day = hour * 24
+
+    // const distance = fimBozo - now;
+    // const timerDays = Math.floor((distance / day) - 31);
+    // const timerHours = Math.floor(distance % day / hour);
+    // const timerMinutes = Math.floor(distance % hour / minute);
+    // const timerSeconds = Math.floor(distance % minute / second)
     const timer = `Faltam ${timerDays} dias, \n${timerHours} ${timerHours === 1 ? 'hora' : 'horas'}, \n${timerMinutes} ${timerMinutes === 1 ? 'minuto' : 'minutos'} e \n${timerSeconds} ${timerSeconds === 1 ? 'segundo' : 'segundos'} \npara o fim do governo Bolsonaro.`
 
+    //Write text on DOM
     document.querySelector('.text').innerText = timer;
 
-    return timerDays;
+    //Adjust bg color
+    document.body.style.backgroundColor = `rgb(255,${timerDays+timerDays/2},${timerDays+timerDays/2})`; 
 }
 
 timerF();
 setInterval(timerF, 1000)
 
-const dayColor = function () {
-    const fimBozo = new Date(2023, 1, 1);
-    
-    const now = new Date();
+// const dayColor = function () {
+//     const fimBozo = new Date('January 01 2023 00:00:00');
+//     const now = new Date();
+//     const diff = fimBozo - now;
 
-    const second = 1000
-    const minute = second * 60
-    const hour = minute * 60
-    const day = hour * 24;
+//     const timerDays = Math.floor(diff/1000/60/60/24);
+//     // const second = 1000
+//     // const minute = second * 60
+//     // const hour = minute * 60
+//     // const day = hour * 24;
 
-    const distance = fimBozo - now;
-    return Math.floor(distance / day);
-}
+//     // const distance = fimBozo - now;
+//     return timerDays;
+// }
 
-document.body.style.backgroundColor = `rgb(255,${dayColor()},${dayColor()})`; 
